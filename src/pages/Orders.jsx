f42e80428b62c7ddc87ea239e3814d887dbb0ece
jawed shiekh <abduljawed6663@gmail.com>
2025-11-05 21:28:06 +0500

    check

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -14,7 +14,7 @@ export default function Orders() {
       customer: "Sara Ahmed",
       date: "2025-10-19",
       total: "$320",
-      status: "Pending",
+      status: "Panding",
     },
     {
       id: "ORD-1003",
